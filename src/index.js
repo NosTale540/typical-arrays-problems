708fb8d43a375da76e364bb1972dd12ff1d8d2c2
Aleksandr <sunseticy@gmail.com>
2020-10-03 11:58:17 +0400

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) {
+        return(0);
+    }
 
-exports.min = function min (array) {
-  return 0;
+    let result = array[0];
+
+    for (i = 1; i < array.length; i++) {
+        if (array[i] < result) {
+            result = array[i];
+        }
+    }
+    return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let result = 0;
+    if (array == undefined || array.length == 0) {
+        return(0);
+    }
+    for (i = 0; i < array.length; i++) {
+        if (array[i] > result) {
+            result = array[i];
+        }
+    }
+    return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) {
+        return(0);
+    }
+
+    let resultAvg = 0;
+
+    for (i = 0; i < array.length; i++) {
+        resultAvg += array[i];
+    }
+    resultAvg = resultAvg / array.length;
+    return resultAvg;
 }
+
